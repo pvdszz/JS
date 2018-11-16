e89eac0ec04d6b48f0734a45e863617f9f91fef5
Phạm Điệp <41050002+pvdszz@users.noreply.github.com>
2018-11-16 09:41:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+/* requirements: 
+    A student management app is able to: 
+    - show current student list
+    - add new students
+*/
+var readlineSync = require('readline-sync');
+var fs = require('fs');
+
+var students = [];
+
+function loadData(){
+    var fileContent = fs.readFileSync('./data.json');
+    students = JSON.parse(fileContent);
+}
+
+function showStudents(){
+    for (var student of students){
+        console.log(student.name, student.age);
+        
+    }
+}
+
+function createStudent(){
+    var name = readlineSync.question('name: ');
+    var age = readlineSync.question('age: ');
+    var student = {
+        name: name,
+        age: parseInt(age)
+    };
+    students.push(student);
+}
+
+function saveAndExit(){
+    var content = JSON.stringify(students);
+    fs.writeFileSync('./data.json', content, {encoding: 'utf8'});
+}
+function showMenu() {
+    console.log('1. Show all students');
+    console.log('2. Create a new student');
+    console.log('3. Save & Exit');
+
+    var option = readlineSync.question('> ');
+    console.log(option);
+    switch(option){
+        case '1':
+            showStudents();
+            showMenu();
+            break;
+        case '2':
+            createStudent();
+            showMenu();
+            break;
+        case '3':
+            saveAndExit();
+            break;
+        default:
+            showMenu();
+            break;
+    }
+}
+
+function main(){
+    loadData();
+    showMenu();
+
+}
+main();
